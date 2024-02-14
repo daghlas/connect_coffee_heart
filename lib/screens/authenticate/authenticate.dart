e19f6ef2394162efd69ca8682b5099dd04bf73a7
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-02-14 15:52:11 +0300

    initial commit

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,18 @@
+import 'package:connect_coffee/screens/authenticate/sign_in.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  const Authenticate({super.key});
+
+  @override
+  State<Authenticate> createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const SignIn(),
+    );
+  }
+}
---
+++ lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,40 @@
+import 'package:connect_coffee/screens/auth.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: const Text('Sign in to connect coffee'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: ElevatedButton(
+          child: const Text('Sign in Anonymously'),
+          onPressed: () async {
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print('Sign in failed');
+            } else {
+              print('Sign in succeeded');
+              print(result);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
