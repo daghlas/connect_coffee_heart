688db5d1d64d94bf3b33b242dc6429157b6683bd
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-02-20 10:34:10 +0300

    spinkit class

---
+++ lib/shared/loading.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.brown[100],
+      child: const Center(
+          child: SpinKitChasingDots(color: Colors.brown, size: 50.0)),
+    );
+  }
+}
