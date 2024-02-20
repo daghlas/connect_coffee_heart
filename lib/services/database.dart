1084ac42bca021b36a159971fc0071c308f4a1fc
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-02-20 14:12:00 +0300

    firestore; databaseCollection

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
+  final String uid;
+  DatabaseService({required this.uid});
+
+  // collection ref
   final CollectionReference connectCollection =
       FirebaseFirestore.instance.collection('Connects');
+
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await connectCollection.doc(uid).set({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+    });
+  }
 }
