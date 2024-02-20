2ce5717a2d2fa16a1c38bbf10f4123aa8ef1c1b7
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-02-20 10:46:19 +0300

    spinkit; register

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:connect_coffee/screens/auth.dart';
 import 'package:connect_coffee/shared/constants.dart';
+import 'package:connect_coffee/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -20,86 +21,97 @@ class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formalKey = GlobalKey<FormState>();
 
+  // spinkit
+  bool loading = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        foregroundColor: Colors.white,
-        elevation: 0.0,
-        title: const Text('Register to Connect Coffee'),
-        actions: <Widget>[
-          ElevatedButton.icon(
-            onPressed: () async {
-              print('Sign In');
-              widget.toggleView();
-            },
-            icon: const Icon(Icons.person),
-            label: const Text('Sign In'),
-            style: ElevatedButton.styleFrom(
+    return loading
+        ? const Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown[100],
+            appBar: AppBar(
               backgroundColor: Colors.brown[400],
-              foregroundColor: Colors.black,
+              foregroundColor: Colors.white,
               elevation: 0.0,
+              title: const Text('Register to Connect Coffee'),
+              actions: <Widget>[
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    print('Sign In');
+                    widget.toggleView();
+                  },
+                  icon: const Icon(Icons.person),
+                  label: const Text('Sign In'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown[400],
+                    foregroundColor: Colors.black,
+                    elevation: 0.0,
+                  ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formalKey,
-          child: Column(children: <Widget>[
-            const SizedBox(height: 20.0),
-            TextFormField(
-              decoration: textInputDecoration.copyWith(hintText: 'Enter Email'),
-              validator: (val) => val!.isEmpty ? 'Enter email' : null,
-              onChanged: (val) {
-                setState(() {
-                  email = val;
-                });
-              },
+            body: Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formalKey,
+                child: Column(children: <Widget>[
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration:
+                        textInputDecoration.copyWith(hintText: 'Enter Email'),
+                    validator: (val) => val!.isEmpty ? 'Enter email' : null,
+                    onChanged: (val) {
+                      setState(() {
+                        email = val;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 20.0),
+                  TextFormField(
+                    decoration: textInputDecoration.copyWith(
+                        hintText: 'Create Password'),
+                    obscureText: true,
+                    validator: (val) =>
+                        val!.length < 6 ? 'Enter valid password' : null,
+                    onChanged: (val) {
+                      setState(() {
+                        password = val;
+                      });
+                    },
+                  ),
+                  const SizedBox(height: 40.0),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (_formalKey.currentState!.validate()) {
+                        setState(() {
+                          loading = true;
+                        });
+                        dynamic result = await _auth
+                            .registerWithEmailAndPassword(email, password);
+                        if (result == null) {
+                          setState(() {
+                            error = 'enter a valid credential';
+                            loading = false;
+                          });
+                        }
+                        print(email);
+                        print(password);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.brown[400]),
+                    child: const Text('Register'),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Text(
+                    error,
+                    style: const TextStyle(color: Colors.red, fontSize: 14.0),
+                  )
+                ]),
+              ),
             ),
-            const SizedBox(height: 20.0),
-            TextFormField(
-              decoration:
-                  textInputDecoration.copyWith(hintText: 'Create Password'),
-              obscureText: true,
-              validator: (val) =>
-                  val!.length < 6 ? 'Enter valid password' : null,
-              onChanged: (val) {
-                setState(() {
-                  password = val;
-                });
-              },
-            ),
-            const SizedBox(height: 40.0),
-            ElevatedButton(
-              onPressed: () async {
-                if (_formalKey.currentState!.validate()) {
-                  dynamic result =
-                      await _auth.registerWithEmailAndPassword(email, password);
-                  if (result == null) {
-                    setState(() {
-                      error = 'enter a valid email';
-                    });
-                  }
-                  print(email);
-                  print(password);
-                }
-              },
-              style:
-                  ElevatedButton.styleFrom(foregroundColor: Colors.brown[400]),
-              child: const Text('Register'),
-            ),
-            const SizedBox(height: 20.0),
-            Text(
-              error,
-              style: const TextStyle(color: Colors.red, fontSize: 14.0),
-            )
-          ]),
-        ),
-      ),
-    );
+          );
   }
 }
