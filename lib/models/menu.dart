72aa19648ce823c1f678326a52451638245c65ae
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-03-07 13:36:16 +0300

    menu model

---
+++ lib/models/menu.dart
@@ -0,0 +1,11 @@
+class Menu {
+  late String name;
+  late String description;
+  late String imagePath;
+
+  Menu({
+    required this.name,
+    required this.description,
+    required this.imagePath,
+  });
+}
--- lib/screens/home/connect_tile.dart
+++ lib/screens/home/connect_tile.dart
@@ -11,15 +11,32 @@ class ConnectTile extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 8.0),
       child: Card(
-        margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
+        color: Colors.white,
+        margin: const EdgeInsets.fromLTRB(13.0, 5.0, 13.0, 0),
         child: ListTile(
+          contentPadding: const EdgeInsets.all(20.0),
           leading: CircleAvatar(
-            radius: 25.0,
+            radius: 30.0,
             backgroundColor: Colors.brown[connectCoffee.strength],
             backgroundImage: const AssetImage('assets/coffee_icon.png'),
           ),
-          title: Text(connectCoffee.name),
-          subtitle: Text('Takes ${connectCoffee.sugars} sugar(s)'),
+          title: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text(
+              connectCoffee.name,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'IBMPlexSans',
+                  letterSpacing: 1.0),
+            ),
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text(
+              'Takes ${connectCoffee.sugars} sugar(s)',
+              style: const TextStyle(color: Colors.grey),
+            ),
+          ),
         ),
       ),
     );
