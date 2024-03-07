19fdce7f261daef8d01ecb5573323e1cee15679b
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-03-07 17:00:31 +0300

    toastMessage

--- lib/screens/pages/cart_page.dart
+++ lib/screens/pages/cart_page.dart
@@ -66,10 +66,10 @@ class _CartPageState extends State<CartPage> {
   void _showToast(BuildContext context) {
     final scaffold = ScaffoldMessenger.of(context);
     scaffold.showSnackBar(
-      SnackBar(
-        content: const Text('Removed from cart'),
-        action: SnackBarAction(
-            label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
+      const SnackBar(
+        content: Text('Removed from cart'),
+        // action: SnackBarAction(
+        //     label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
       ),
     );
   }
--- lib/screens/pages/menu_page.dart
+++ lib/screens/pages/menu_page.dart
@@ -66,10 +66,10 @@ class _MenuPageState extends State<MenuPage> {
   void _showToast(BuildContext context) {
     final scaffold = ScaffoldMessenger.of(context);
     scaffold.showSnackBar(
-      SnackBar(
-        content: const Text('Added to cart'),
-        action: SnackBarAction(
-            label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
+      const SnackBar(
+        content: Text('Added to cart'),
+        // action: SnackBarAction(
+        //     label: 'View Cart', onPressed: scaffold.hideCurrentSnackBar),
       ),
     );
   }
