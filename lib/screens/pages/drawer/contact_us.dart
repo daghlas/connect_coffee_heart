d65362b0bc527b77847bd7f30c92232830e0ecb0
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-03-18 11:43:56 +0300

    contact_us

--- lib/screens/pages/drawer/contact_us.dart
+++ lib/screens/pages/drawer/contact_us.dart
@@ -11,23 +11,60 @@ class _ContactUsState extends State<ContactUs> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.brown,
       appBar: AppBar(
         backgroundColor: Colors.brown,
         foregroundColor: Colors.white,
         title: const Text('Contact Us'),
       ),
-      body: Container(
-          color: Colors.grey[350],
-          child: const Center(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 70.0),
               child: Text(
-            'CONTACT US PAGE',
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'IBMPlexSans',
+                'HEADER IMAGE',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'IBMPlexSans',
+                ),
+              ),
             ),
-          ))),
+          ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.only(top: 100.0),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60))),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Text(
+                        'BODY',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'IBMPlexSans',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
