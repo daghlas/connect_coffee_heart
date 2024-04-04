65bd71cb2fcb193c09b2ba4720dc9b39264a8892
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-04-04 13:09:36 +0300

    itemSlide

---
+++ lib/screens/home/menu_item_slide.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemSlide extends StatefulWidget {
+  const ItemSlide({super.key});
+
+  @override
+  State<ItemSlide> createState() => _ItemSlideState();
+}
+
+class _ItemSlideState extends State<ItemSlide> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
