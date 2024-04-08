329b2e33becc93d6423df47f1f8a3b9dfd2a474c
Daghlas Kaire <daghlaskaire58@gmail.com>
2024-04-08 16:27:44 +0300

    favourites

--- lib/screens/pages/bottom_nav/favourites_page.dart
+++ lib/screens/pages/bottom_nav/favourites_page.dart
@@ -1,4 +1,8 @@
+import 'package:connect_coffee/components/menu_item_tile.dart';
+import 'package:connect_coffee/models/connect_menu.dart';
+import 'package:connect_coffee/models/menu.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class FavouritesPage extends StatefulWidget {
   const FavouritesPage({super.key});
@@ -8,63 +12,105 @@ class FavouritesPage extends StatefulWidget {
 }
 
 class _FavouritesPageState extends State<FavouritesPage> {
+  // remove item to cart
+  void removeFromCart(MenuItems menuItems) {
+    Provider.of<ConnectMenu>(context, listen: false)
+        .removeItemFromCart(menuItems);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(80.0, 25.0, 80.0, 0),
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Text(
-                'FAVOURITES',
-                style: TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                  // fontFamily: 'IBMPlexSans',
+    return Consumer<ConnectMenu>(
+      builder: (BuildContext context, ConnectMenu value, Widget? child) =>
+          SafeArea(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(80.0, 25.0, 80.0, 0),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  'FAVOURITES',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                    // fontFamily: 'IBMPlexSans',
+                  ),
                 ),
               ),
             ),
-          ),
 
-          const SizedBox(height: 15.0),
+            const SizedBox(height: 15.0),
 
-          //list of menu items
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(34.0)),
-              ),
+            //list of menu items
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.only(topLeft: Radius.circular(34.0)),
+                ),
 
-              // CONTENT GOES HERE
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(20.0, 30.0, 0, 5.0),
-                    child: Text(
-                      'Your preferences - Favourites',
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.bold),
+                // CONTENT GOES HERE
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(20.0, 30.0, 0, 5.0),
+                      child: Text(
+                        'Your preferences - Favourites',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                  ListView.builder(
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        // code goes here
-                      }),
-                ],
+                    ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: value.userCart.length,
+                        itemBuilder: (context, index) {
+                          // get individual menu item
+                          MenuItems menuItem = value.userCart[index];
+
+                          // return the tile for the item
+                          return MenuItemTile(
+                            menuItems: menuItem,
+                            onPressed: () {
+                              removeFromCart(menuItem);
+                              _showToast(context);
+                            },
+                            icon: const Icon(Icons.delete),
+                          );
+                        }),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  // toast message
+  void _showToast(BuildContext context) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(
+      const SnackBar(
+        content: Center(
+            child: Text(
+          'REMOVED FROM CART',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontFamily: 'IBMPlexSans',
+          ),
+        )),
+        backgroundColor: Colors.black,
+        // action: SnackBarAction(
+        //     label: 'UNDO', onPressed: scaffold.hideCurrentSnackBar),
       ),
     );
   }
